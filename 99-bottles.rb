c6d8b4f452461d6944646687af366df656365090
Bjorn Linder <bjornishungry@hotmail.com>
2013-11-18 16:37:29 -0600

    Update 99-bottles.rb

--- 99-bottles.rb
+++ 99-bottles.rb
@@ -2,10 +2,11 @@ puts'Would you like to hear an awful song?'
 response=gets.chomp
 if response == 'yes' || response == 'Yes'
   count=99
+  puts'You asked for it.'
   while count!=1
-    puts count.to_s + 'bottles of beer on the wall'+count.to_s+'bottles of beer.'
+    puts count.to_s + ' bottles of beer on the wall, '+count.to_s+' bottles of beer.'
     count=count-1
-    puts'Take one down, pass it around,'+count.to_s+'bottles of beer on the wall.'
+    puts'Take one down, pass it around,'+count.to_s+' bottles of beer on the wall.'
 end
 else
   puts'Good choice.'
