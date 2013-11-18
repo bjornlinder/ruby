3e7809aafea8d455cc0774a22917c03e5efbed1e
Bjorn Linder <bjornishungry@hotmail.com>
2013-11-18 16:27:03 -0600

    Update 99-bottles.rb

--- 99-bottles.rb
+++ 99-bottles.rb
@@ -1,9 +1,9 @@
 puts'would you like to hear an awful song?'
 response=gets.chomp
-if response == 'yes' || 'Yes'
+if response == 'yes' || response == 'Yes'
   count=99
   while count!=1
-    puts count+'bottles of beer on the wall'+count+'bottles of beer.
+    puts count+'bottles of beer on the wall'+count+'bottles of beer.'
     count=count-1
     puts'Take one down, pass it around,'+count+'bottles of beer on the wall.'
 else
