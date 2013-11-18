b8de176c9616f50364a093eb86725d68c33ff4df
Bjorn Linder <bjornishungry@hotmail.com>
2013-11-18 16:38:18 -0600

    Update 99-bottles.rb

--- 99-bottles.rb
+++ 99-bottles.rb
@@ -6,7 +6,7 @@ if response == 'yes' || response == 'Yes'
   while count!=1
     puts count.to_s + ' bottles of beer on the wall, '+count.to_s+' bottles of beer.'
     count=count-1
-    puts'Take one down, pass it around,'+count.to_s+' bottles of beer on the wall.'
+    puts'Take one down, pass it around, '+count.to_s+' bottles of beer on the wall.'
 end
 else
   puts'Good choice.'
