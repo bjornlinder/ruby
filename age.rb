d8abcd8e3b325894566accfdb1a8306c1aa2be43
Bjorn Linder <bjornishungry@hotmail.com>
2013-11-25 13:25:22 -0600

    Update age.rb

--- age.rb
+++ age.rb
@@ -3,7 +3,10 @@
 birth=Time.new(1989,7,13)
 birth.nsec
 current=Time.now
-puts((birth-current)/60/60/24/365)
+age=(birth-current)/60/60/24/365
+puts(age)
+#24 years old and how many days?
+puts(age.to_int-age)*365
 #What day was I born on?
 puts(birth.wday)
 puts(current.wday)
