1ba75148b2bcf8e1c002026d6ad2016fc3659e15
Bjorn Linder <bjornishungry@hotmail.com>
2013-11-25 13:06:49 -0600

    Create age.rb

---
+++ age.rb
@@ -0,0 +1,14 @@
+#Chapter 4 Exercise, Your Age in Seconds
+
+birth=Time.new(1989,7,13)
+birth.nsec
+current=Time.now
+puts(birth-current)
+#What day was I born on?
+birth.monday
+birth.tuesday
+birth.wednesday
+birth.thursday
+birth.friday
+birth.saturday
+birth.sunday
