fe111c06d5fbccf6f7623cbe77f059e1a3073498
Bjorn Linder <bjornishungry@hotmail.com>
2013-12-18 16:47:32 -0600

    Update music.rb

--- music.rb
+++ music.rb
@@ -2,4 +2,6 @@
 #This is my own Ch. 13 exercise, idea taken from one of the sample exercises. 
 #Goal is to sort music based on which songs already exist in another directory (and subdirectories), such that only songs without duplicates are copied over.
 #Why? Because iTunes sucks at checking for duplicates. I could also change the program to do other cool stuff with my music collection.
-
+#Like for example adding 'artist' to the 'artist' slot of the music files from where it is....
+songs=Dir[C:/Documents/Music/Brian/acdc/'**/*.{mp3}']
+puts songs.to_s
