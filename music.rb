6d781d4e27ca4544b562d1ef81f675985ba183c6
Bjorn Linder <bjornishungry@hotmail.com>
2013-12-18 16:38:41 -0600

    Create music.rb
    Most ambitious program yet, so this one will take some more time. Assuming it's successful, this will also be the first useful program I write.
    This is my own Ch. 13 exercise, idea taken from one of the sample exercises. 
    Goal is to sort music based on which songs already exist in another directory (and subdirectories), such that only songs without duplicates are copied over.
    Why? Because iTunes sucks at checking for duplicates. I could also change the program to do other cool stuff with my music collection.

---
+++ music.rb
@@ -0,0 +1,5 @@
+#Most ambitious program yet, so this one will take some more time. Assuming it's successful, this will also be the first useful program I write.
+#This is my own Ch. 13 exercise, idea taken from one of the sample exercises. 
+#Goal is to sort music based on which songs already exist in another directory (and subdirectories), such that only songs without duplicates are copied over.
+#Why? Because iTunes sucks at checking for duplicates. I could also change the program to do other cool stuff with my music collection.
+
